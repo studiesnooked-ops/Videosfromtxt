605538cdd59692d7a410f2e0a56def0a473bac85
studiesnooked-ops <studiesnooked@gmail.com>
2026-06-06 16:08:49 +0530

    Delete handlers directory

--- handlers/start.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import filters
-
-def register_start(app):
-    @app.on_message(filters.command("start"))
-    async def start(_, message):
-        await message.reply("Send a TXT file containing video URLs.")
